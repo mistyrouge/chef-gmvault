33677f08fd94a37f8ea60214f99f167606650373
Dorian Jaminais <dorian@jaminais.fr>
2015-07-22 06:57:50 +0100

    Add symlink to /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games and create gmvault group

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,22 +2,28 @@
 # Cookbook Name:: gmvault
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
+# Do whatever the fuck you want
 #
 
 include_recipe 'python'
 
-root = "/usr/lib/gmvault"
+root = "/usr/local/lib/gmvault"
+
+users_manage "gmvault" do
+  action [ :remove, :create ]
+end
 
 python_virtualenv root do
   owner "root"
-  group "root"
+  group "gmvault"
   action :create
 end
 
 python_pip "gmvault" do
   virtualenv root
   action [:install, :upgrade]
+end
+
+link '/usr/local/bin/gmvault' do
+  to "#{root}/bin/gmvault"
 end
