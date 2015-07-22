5a5f8ead94acba51869d0fe3ad5abbb619ee46aa
Dorian Jaminais <dorian@jaminais.fr>
2015-07-22 21:56:19 +0100

    Fix multiline strings

--- recipes/cron_users.rb
+++ recipes/cron_users.rb
@@ -8,8 +8,8 @@
 include_recipe 'gmvault'
 
 if Chef::Config[:solo] and not chef_solo_search_installed?
-    Chef::Log.warn("This recipe uses search. Chef Solo does not support search "
-                   "unless you install the chef-solo-search cookbook.")
+    Chef::Log.warn("This recipe uses search. Chef Solo does not support "\
+                   "search unless you install the chef-solo-search cookbook.")
 else
     search('users', 'groups:gmvault AND email:*') do |user|
 
@@ -25,8 +25,8 @@ else
             command "gmvault sync #{user['id']} -d ~/email"
         end
 
-        Chef::Log.warn("You will need to run gmvault manually for the first "
-                       "time to authenticate yourself.\n"
+        Chef::Log.warn("You will need to run gmvault manually for the first "\
+                       "time to authenticate yourself.\n"\
                        "$ gmvault sync #{user['id']} -d ~/email")
 
       end
