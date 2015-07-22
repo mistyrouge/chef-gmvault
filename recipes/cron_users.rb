b5a03dc45892d6a6ceb0c74bb130ed6fcea98ec8
Dorian Jaminais <dorian@jaminais.fr>
2015-07-22 08:17:10 +0100

    Create a new recipe to set up cron backups

---
+++ recipes/cron_users.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: gmvault
+# Recipe:: default
+#
+# Do whatever the fuck you want
+#
+
+include_recipe 'gmvault'
+
+if Chef::Config[:solo] and not chef_solo_search_installed?
+    Chef::Log.warn("This recipe uses search. Chef Solo does not support search "
+                   "unless you install the chef-solo-search cookbook.")
+else
+    search('users', 'groups:gmvault AND email:*') do |user|
+
+        user['username'] ||= user['id']
+
+        cron 'gmvault' do
+            action :create
+            minute '45'
+            hour '7'
+            day '*'
+            user user['username']
+            mailto user['email']
+            command "gmvault sync #{user['id']} -d ~/email"
+        end
+
+        Chef::Log.warn("You will need to run gmvault manually for the first "
+                       "time to authenticate yourself.\n"
+                       "$ gmvault sync #{user['id']} -d ~/email")
+
+      end
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,20 +10,20 @@ include_recipe 'python'
 root = "/usr/local/lib/gmvault"
 
 users_manage "gmvault" do
-  action [ :remove, :create ]
+    action [ :remove, :create ]
 end
 
 python_virtualenv root do
-  owner "root"
-  group "gmvault"
-  action :create
+    owner "root"
+    group "gmvault"
+    action :create
 end
 
 python_pip "gmvault" do
-  virtualenv root
-  action [:install, :upgrade]
+    virtualenv root
+    action [:install, :upgrade]
 end
 
 link '/usr/local/bin/gmvault' do
-  to "#{root}/bin/gmvault"
+    to "#{root}/bin/gmvault"
 end
